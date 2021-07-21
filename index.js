cf3c33b14afa69f60939002af33a7161949f70ea
esellers25 <erin.e.sellers@gmail.com>
2021-07-21 14:50:58 -0400

    Done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function distanceFromHqInBlocks(blockNumber) {
-    if (blockNumber>=42) {
-        return blockNumber - 42; 
-    } else if (blockNumber<42) {
-        return 42 - blockNumber;
-    }
+   
+    let distance 
+
+    if (blockNumber >= 42) {
+       distance = blockNumber - 42
+   }
+   else {
+       distance = 42 - blockNumber
+   }
+
+   return distance; 
 }
 
 function distanceFromHqInFeet(distance) {
@@ -11,21 +17,29 @@ function distanceFromHqInFeet(distance) {
 }
 
 function distanceTravelledInFeet(block1, block2) {
-    if (block1 < block2) {
-        return (block2 - block1) * 264;
-     } else (block1 > block2)
-        return (block1 - block2) * 264;
+    let distance
+    if (block1 >= block2){
+        distance = block1 - block2
+        return distance * 264
+    }
+    else {
+        distance = block2 - block1
+        return distance * 264
+    }
     }
 
 function calculatesFarePrice(block1, block2) {
-    let result = distanceTravelledInFeet(block1, block2);
-    if (result<400) {
+    let distance = distanceTravelledInFeet(block1, block2)
+    if (distance < 400){
         return 0;
-    } else if (result > 400 && result <= 2000) {
-        return (result - 400) * .02;
-    } else if (result > 2000 && result < 2500) {
-        return 25
-    } else if (result >= 2500) {
-        return "cannot travel that far"
+    }
+    else if (distance >= 400 && distance < 2000){
+        return (distance - 400) * .02;
+    }
+    else if (distance >= 2000 && distance < 2500){
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
     }
 }
